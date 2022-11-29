546f3c785af91c6e40d24732ebbabb416fbb343d
Bruno Marcenaro <bems2588@gmail.com>
2022-11-28 21:05:29 -0500

    check btn

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-'use strict';
-console.log(document.querySelector('.message').textContent);
+// 'use strict';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = "Correct number!"
+// document.querySelector('.message').textContent = "Correct number!"
 
-document.querySelector('.number').textContent =  13;
-document.querySelector('.score').textContent =  20;
-document.querySelector('.guess').value =  23;
+// document.querySelector('.number').textContent =  13;
+// document.querySelector('.score').textContent =  20;
+// document.querySelector('.guess').value =  23;
 
+document.querySelector('.check').addEventListener('click', function(){
+  console.log(document.querySelector('.guess').value);
+});
