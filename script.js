680b2cc916edd1a1f537231e625b52a7e05a3450
Bruno Marcenaro <bems2588@gmail.com>
2022-11-28 20:34:46 -0500

    adding values

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = "Correct number!"
+
+document.querySelector('.number').textContent =  13;
+document.querySelector('.score').textContent =  20;
+document.querySelector('.guess').value =  23;
+
