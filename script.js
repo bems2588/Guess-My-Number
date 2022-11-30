41803365969d13f4d3b8934b9bb56142900fd0f8
Bruno Marcenaro <bems2588@gmail.com>
2022-11-30 06:58:40 -0500

    else

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
 // document.querySelector('.number').textContent =  13;
 // document.querySelector('.score').textContent =  20;
 // document.querySelector('.guess').value =  23;
+const number = Math.random();
+
+console.log(number);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,8 +17,6 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number 😫!'
-  } else {
-    document.querySelector('.message').textContent = "Correct number!";
-  }
+  } 
 
 })
