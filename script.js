54a5d9ffaccb0cf2f79e866f061283bb7c495078
Bruno Marcenaro <bems2588@gmail.com>
2022-12-01 06:49:05 -0500

    hide secretNumber

--- script.js
+++ script.js
@@ -9,9 +9,6 @@
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-console.log(secretNumber);
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -24,7 +21,11 @@ document.querySelector('.check').addEventListener('click', function () {
     //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!!!'
+    document.querySelector('.number').textContent = secretNumber;
+    console.log(secretNumber);
     document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+
     //When number is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
