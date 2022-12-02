716a5036d388ea1526529b52a5d02836701df519
Bruno Marcenaro <bems2588@gmail.com>
2022-12-01 20:55:25 -0500

    reset values

--- script.js
+++ script.js
@@ -51,15 +51,15 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 })
 
-document.querySelector('.btn again').addEventListener('click', function () {
+document.querySelector('.again').addEventListener('click', function () {
 
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector('.message').textContent = "Start guessing...";
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').textContent = '';
-  document.querySelector('body').style.backgroundColor = 'black';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 
 
