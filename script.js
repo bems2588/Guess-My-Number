f5bea2ec0f92bac83d7ca0ad7c41a58e47d3f5fb
Bruno Marcenaro <bems2588@gmail.com>
2022-12-02 06:45:48 -0500

    add highScore

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
 // document.querySelector('.guess').value =  23;
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,7 +26,9 @@ document.querySelector('.check').addEventListener('click', function () {
     console.log(secretNumber);
     document.querySelector('body').style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
-
+    if (score > highScore) {
+      highScore = score;
+    }
     //When number is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -61,6 +64,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.guess').value = '';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
-
-
 });
+
